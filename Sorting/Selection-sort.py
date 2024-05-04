921859fae8e5a63e457ab636816cac3c40558194
thegoutampatel <goutampatel9131@gmail.com>
2024-05-04 20:12:24 +0530

    selection-sort

---
+++ Sorting/Selection-sort.py
@@ -0,0 +1,14 @@
+
+def SelectionSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i]>arr[j]:
+                arr[i],arr[j] = arr[j],arr[i]
+    return arr
+
+
+#driver code 
+arr = [70,50,20,30,90,5,15]
+result = SelectionSort(arr)
+print("The Arry after Selection sort : ",result)
