b3985cd30517f37b8680db2cb61b2ef727ca4e90
thegoutampatel <goutampatel9131@gmail.com>
2024-06-12 08:41:20 +0530

    assignment of Oops

---
+++ Oops/Assignments.py
@@ -0,0 +1,56 @@
+
+                       #1
+#QUE: Create a Class and get the name of 3 students with his marks and define 
+# a function to get the avg of his 3 subjects marks
+
+# class Students:
+#     def __init__(self,name,marks1,marks2,marks3):
+#         self.name = name
+#         self.marks1 = marks1
+#         self.marks2 = marks2
+#         self.marks3 = marks3
+    
+#     def Avg(self):
+#         return (self.marks1 + self.marks2 + self.marks3) / 3
+
+
+# s1 = Students("Goutam",70,90,80)
+# s2 = Students("Yogesh",60,50,40)
+# s3 = Students("Manish", 70,60,40)
+
+# print(s1.name,s1.Avg())
+# print(s2.name,s2.Avg())
+# print(s3.name,s3.Avg())
+
+                    #02
+
+#QUE : create a account with 2 Attributes - balance and acc.No
+    # Crete a method for debit credit and print balace
+
+# class account:
+#     def __init__(self,acc_No, balance):
+#         self.acc_No = acc_No
+#         self.balance = balance
+        
+#     def debit(self,amt):
+#         self.balance -= amt
+#         print("Rs" , amt , "is debit from your account")
+#         print("Total balance ", self.print_balance())
+
+#     def credit(self,amt):
+#         self.balance += amt
+#         print("Rs" , amt , "is credit in your account")
+#         print("Total balance ", self.print_balance())
+
+#     def print_balance(self):
+#         return self.balance
+        
+
+# a1 = account(123456789, 200)
+
+# a1.debit(100)
+# a1.credit(500)
+
+
+
+
